27ae6b0b7d9f7da8f62bfd2af9730f87a06c3cde
GabrieleLerani <92364167+GabrieleLerani@users.noreply.github.com>
2023-03-11 21:09:49 +0100

    Now the code is formatted correctly

--- AuthenticatedLink-v2.py
+++ AuthenticatedLink-v2.py
@@ -1,18 +1,20 @@
 import socket
 import hashlib
 import hmac
+import json
 from threading import Thread
 
 from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
 
 RCV_BUFFER_SIZE = 1024
 
+
 class AuthenticatedLink:
-    def __init__(self, selfid, selfip, id, ip, proc):
+    def __init__(self, self_id, self_ip, id, ip, proc):
         self.proc = proc
-        self.selfid = selfid   #id of sending process
-        self.id = id           #id of receiving process
-        self.selfip = selfip
+        self.self_id = self_id  # id of sending process
+        self.id = id  # id of receiving process
+        self.self_ip = self_ip
         self.ip = ip
         self.key = []
 
@@ -20,13 +22,14 @@ class AuthenticatedLink:
         print("Start thread to receive messages...")
         t = Thread(target=self.__receive)
         t.start()
-	
-	# This handles the message receive
-	# Now the listening port is the concatenation 50/5 - 'receiving process' - 'sending process'
+
+    # This handles the message receive
+    # Now the listening port is the concatenation 50/5 - 'receiving process' - 'sending process'
     def __receive(self):
         host = ''  # Symbolic name meaning all available interfaces
         # It uses ternary operator
-        port = int("50" + self.id + self.selfid ) if self.selfid < 10 and self.id < 10 else int("5" + self.id + self.selfid)    
+        port = int("50" + self.id + self.self_id) if self.self_id < 10 and self.id < 10 else int(
+            "5" + self.id + self.self_id)
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((host, port))
             s.listen(0)
@@ -36,58 +39,60 @@ class AuthenticatedLink:
                 while True:
                     data = conn.recv(RCV_BUFFER_SIZE)
                     if not data: break
-                    paresd_data = json.loads(data.decode())
+                    parsed_data = json.loads(data.decode())
                     print(parsed_data)
                     self.__deliver(data, id)
-	
-	# DICTIONARY FOR KEY ??
+
+    # DICTIONARY FOR KEY ??
     def __check(self, id):
         if self.key[id] == 0:
             self.key[id] = ChaCha20Poly1305.generate_key()
             self.s.sendall(self.key[id])
             self.temp = self.s.recv(RCV_BUFFER_SIZE, 0)
-            if self.temp != "synACK":   #ack usato per sincronia con altro processo
+            if self.temp != "synACK":  # Ack used for synchronization with other process
                 return 1
-                
-	# Compute the hmac of the message with a key associated to the process with self.id
-	# The message is returned as a dictionary: {"MSG": message,"HMAC": hmac, "FLAG": flag}
-	# The hmac is computed starting from the concatenation of flag and message
-	# Example: flag = "SEND" , message = "Hello" ----> HMAC("SENDHello")
+
+    # Compute the hmac of the message with a key associated to the process with self.id
+    # The message is returned as a dictionary: {"MSG": message,"HMAC": hmac, "FLAG": flag}
+    # The hmac is computed starting from the concatenation of flag and message
+    # Example: flag = "SEND" , message = "Hello" ----> HMAC("SENDHello")
     def __auth(self, message, flag):
         self.__check(self.id)
-        mess = {"MSG":message, "HMAC":hmac.new(self.key[self.id], flag+message, hashlib.sha256, "FLAG":flag ).hexdigest()}  
+        mess = {"MSG": message,
+                "HMAC": hmac.new(self.key[self.id], flag + message, hashlib.sha256).hexdigest(),
+                "FLAG": flag}
         return mess
-	
-	
-	# The send open a new socket, the port is the concatenation of 50/5- id of sending process - id of receiving process
-	# Example: sending_id = 1, receiving_id = 2 ---> port = 5012
-	def send(self, message, flag):
-		# It uses ternary operator
-		port = int("50" + self.selfid + self.id) if self.selfid < 10 and self.id < 10 else int("5" + self.selfid + self.id)    
-    	
+
+    # The send open a new socket, the port is the concatenation of 50/5- id of sending process - id of receiving process
+    # Example: sending_id = 1, receiving_id = 2 ---> port = 5012
+    def send(self, message, flag):
+        # It uses ternary operator
+        port = int("50" + self.self_id + self.id) if self.self_id < 10 and self.id < 10 else int(
+            "5" + self.self_id + self.id)
+
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.s:
             self.s.connect((self.ip, port))
-        
+
         # Mess is a dictionary
         mess = self.__auth(message, flag)
         parsed_data = json.dumps(mess)
-        self.s.sendall(bytes(parsed_data,encoding='utf-8'))
+        self.s.sendall(bytes(parsed_data, encoding='utf-8'))
         self.s.close()
-	
-	# It checks message authenticity comparing the hmac
+
+    # It checks message authenticity comparing the hmac
     def __check_auth(self, message, hmac, flag, id):
-		temphash = hmac.new(self.key[id], temp+message, hashlib.sha256).hexdigest()   #creazione HMAC dal messaggio contenuto in SEND,message(lista)
-        return temphash == hhmac
+        temp_hash = hmac.new(self.key[id], flag + message, hashlib.sha256).hexdigest()
+        return temp_hash == hmac
 
     def __deliver(self, message, id):
-        
+
         msg = message["MSG"]
-    	hmac = message["HMAC"]
-    	flag = message["FLAG"]
-        
+        hmac = message["HMAC"]
+        flag = message["FLAG"]
+
         self.__check_auth(message, hmac, flag, id)
-    	
-    	if flag == "SEND":   
+
+        if flag == "SEND":
             self.proc.deliverSend(msg, flag, self.id)
         elif flag == "ECHO":
             self.proc.deliverEcho(msg, flag, self.id)
