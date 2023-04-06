69f9ad65f9c26dcfdee5bb39540fd887be2a55b1
CarloGiralda <92364122+CarloGiralda@users.noreply.github.com>
2023-04-06 13:57:44 +0200

    Add files via upload

---
+++ QueueHashBased/main.py
@@ -0,0 +1,16 @@
+import time
+import Process
+
+TIME_SLEEP = 10
+
+Role = "Broadcaster"
+#Role = "Receiver"
+
+if __name__ == '__main__':
+    p = Process.Process()
+    p.connection_to_server()
+    p.creation_links()
+    if Role == "Broadcaster":
+        time.sleep(TIME_SLEEP)
+        p.broadcast("Hello!")
+    exit(0)
