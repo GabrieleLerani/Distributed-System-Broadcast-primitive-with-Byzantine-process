a42b8c088b28099b5be29e82d4b9589c34e5ac0e
GabrieleLerani <92364167+GabrieleLerani@users.noreply.github.com>
2023-05-04 00:14:41 +0200

    Add files via upload

---
+++ Mininet simulation/Bracha-BRB/AuthenticatedLink.py
@@ -0,0 +1,216 @@
+
+import socket
+import hashlib
+import hmac
+import json
+import logging
+from threading import Thread
+import threading
+import time
+
+import utils
+
+
+from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
+
+RCV_BUFFER_SIZE = 8192
+KEY_SIZE = 32
+
+
+class AuthenticatedLink:
+    def __init__(self, self_id, self_ip, idn, ip, proc):
+        self.proc = proc
+        self.self_id = self_id  # id of sending process
+        self.id = idn  # id of receiving process
+        self.self_ip = self_ip
+        self.ip = ip
+        self.key = {}
+        self.sending_port = (
+            int("50" + str(self.self_id) + str(self.id))
+            if self.self_id < 10 and self.id < 10
+            else int("5" + str(self.self_id) + str(self.id))
+        )
+        self.receiving_port = (
+            int("50" + str(self.id) + str(self.self_id))
+            if self.self_id < 10 and self.id < 10
+            else int("5" + str(self.id) + str(self.self_id))
+        )
+
+    # TODO 
+    def key_exchange(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.sock:
+            self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            while True:
+                # Try Except used to repeat the connection until the other socket is opened again
+                try:
+                    self.sock.connect((self.ip, self.sending_port))
+
+                    self.__check(self.sock, self.id)
+
+                    break
+                except Exception as e:
+                    print(f"Exception 1, {e}")
+                    break
+
+    def receiver(self):
+        logging.info("AUTH:Start thread to receive messages...")
+        t = Thread(target=self.__receive)
+        t.start()
+
+    # This handles the message receive
+    # Now the listening port is the concatenation 50/5 - 'receiving process' - 'sending process'
+    def __receive(self):
+        host = ''  # Symbolic name meaning all available interfaces
+        #host = utils.get_ip_of_interface()
+        # It uses ternary operator
+        
+
+        logging.info("AUTH:Port used for receiving: %d", self.receiving_port)
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.s:
+            self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.s.bind(('', self.receiving_port))
+            self.s.listen(100)
+            print(f"listening for connection on {host,self.receiving_port}")
+            conn, addr = self.s.accept()
+            
+            with conn:
+                logging.info("AUTH:Connected by %s", addr)
+                while True:
+                    data = conn.recv(RCV_BUFFER_SIZE)
+
+                    if not data:
+                        break
+
+                    parsed_data = json.loads(data.decode())
+
+                    logging.info(
+                        "----- EVALUATION CHECKPOINT: message receiving, time: %s -----",
+                        time.time() * 1000,
+                    )
+                    logging.info(
+                        "AUTH: <%s, %d> -- sent this data %s",
+                        self.ip,
+                        self.id,
+                        parsed_data,
+                    )
+
+                    # Useful for performance evaluation, upon the arrival of the first
+                    # message it starts timer to monitor execution time
+
+                    if "MSG" not in parsed_data.keys():
+                        self.__add_key(parsed_data)
+                        conn.sendall(b"synACK")
+                    else:
+                        t = Thread(
+                            target=self.__deliver,
+                            args=(parsed_data, threading.currentThread()),
+                        )
+                        t.start()
+
+                    logging.info("AUTH:Received DATA: %s", data)
+
+                logging.info(
+                    "AUTH:------- SOCKET CLOSED, ME: %s,TO: %s", self.self_ip, self.ip
+                )
+
+    def __add_key(self, key_dict):
+        self.key[self.id] = key_dict["KEY"].encode("latin1")
+        
+        logging.info(
+            "AUTH: <%s, %d> is the one with this key: %s", self.ip, self.id, self.key
+        )
+
+    def __check(self, sock, idn):
+        #if idn not in self.key and self.self_id <= idn:
+        if idn not in self.key: 
+            self.key[idn] = ChaCha20Poly1305.generate_key()
+
+            key_to_send = {"KEY": self.key[idn].decode("latin1")}
+            logging.info("AUTH: Key generated")
+            logging.info("AUTH: This is what I am sending: %s", key_to_send)
+
+            data = json.dumps(key_to_send)
+            sock.sendall(data.encode())
+            temp = sock.recv(RCV_BUFFER_SIZE, 0).decode()
+            
+            if temp != "synACK":  # Ack used for synchronization with other process
+                return 1
+
+    # Compute the hmac of the message with a key associated to the process with self.id
+    # The message is returned as a dictionary: {"MSG": message,"HMAC": hmac, "FLAG": flag}
+    # The hmac is computed starting from the concatenation of flag and message
+    # Example: flag = "SEND" , message = "Hello" ----> HMAC("SENDHello")
+    def __auth(self, sock, message):
+        self.__check(sock, self.id)
+        mess = {
+            "MSG": message["MSG"],
+            "HMAC": hmac.new(
+                self.key.get(self.id, "Key not found"),
+                (message["FLAG"] + message["MSG"]).encode("utf-8"),
+                hashlib.sha256,
+            ).hexdigest(),
+            "FLAG": message["FLAG"],
+        }
+
+        return mess
+
+    # The send open a new socket, the port is the concatenation of 50/5- id of sending process - id of receiving process
+    # Example: sending_id = 1, receiving_id = 2 ---> port = 5012
+    def send(self, message):
+        # It uses ternary operator
+
+        logging.info(
+            "AUTH: Port used to connect: %d to <%s,%d>", self.sending_port, self.ip, self.id
+        )
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            
+            while True:
+                try:
+                    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                    sock.connect((self.ip, self.sending_port))
+                    
+                    # Mess is a dictionary
+                    mess = self.__auth(sock, message)
+                    parsed_data = json.dumps(mess)
+                    sock.sendall(bytes(parsed_data, encoding="utf-8"))
+                    logging.info("AUTH: %s sent to <%s, %d>", mess, self.ip, self.id)
+                    break
+                except Exception as e:
+                     print(f"Exception {e} {message}")
+                     break
+                
+                    
+
+                
+
+    # It checks message authenticity comparing the hmac
+    def __check_auth(self, message):
+        temp_hash = hmac.new(
+            self.key.get(self.id, "Key not found"),
+            (message["FLAG"] + message["MSG"]).encode("utf-8"),
+            hashlib.sha256,
+        ).hexdigest()
+        return temp_hash == message["HMAC"]
+
+    def __deliver(self, message, t):
+        flag = message["FLAG"]
+
+        if not self.__check_auth(message):
+            logging.info("--- Authenticity check failed for %s", message)
+            print("Authenticity check failed")
+            return
+
+        if flag != "READY":
+            t.join()
+            self.receiver()
+
+        message.pop("HMAC", None)
+
+        if flag == "SEND":
+            self.proc.deliver_send(message, self.id)
+        elif flag == "ECHO":
+            self.proc.deliver_echo(message, self.id)
+        elif flag == "READY":
+            self.proc.deliver_ready(message, self.id)
---
+++ Mininet simulation/Bracha-BRB/Process.py
@@ -0,0 +1,178 @@
+import math
+import utils
+import AuthenticatedLink
+import threading
+from threading import Thread
+import time
+import logging
+import Evaluation
+
+
+BREAK_TIME = 0.08
+
+
+class Process:
+    def __init__(self):
+        self.ids = []
+        self.ips = []
+        self.currentMSG = []
+        self.selfip = 0
+        self.selfid = 0
+        self.AL = []
+        self.sentecho = False
+        self.sentready = False
+        self.delivered = False
+        self.echos = {}
+        self.readys = {}
+        self.faulty = 0
+        self.eval = Evaluation.Evaluation()
+
+    def init_process(self):
+        # Start tracing memory for statistics
+        self.eval.tracing_start()
+        # Populate ids ips from file
+        self.init_process_ids()
+        logging.debug("PROCESS: id list: %s,ip list %s", self.ids, self.ips)
+        print("-----GATHERED ALL THE PEERS IPS AND IDS-----")
+        print("-----STARTING SENDING OR RECEIVING MESSAGES-----")
+        t = Thread(target=self.__thread)
+        t.start()
+
+    def init_process_ids(self):
+        processes = utils.read_process_identifier()
+        for pair in processes:
+            self.ids.append(int(pair[0]))  # pair[0] --> ID
+            self.ips.append(pair[1])  # pair[1] --> IPS
+
+    # Without server is enough to make self.AL[i].receiver() once
+    def creation_links(self):
+        self.selfip = utils.get_ip_of_interface()
+        self.selfid = self.ids[self.ips.index(self.selfip)]
+        self.barrier = threading.Barrier(parties=2)
+
+        for i in range(0, len(self.ids)):
+            self.AL.append(
+                AuthenticatedLink.AuthenticatedLink(
+                    self.selfid,
+                    self.selfip,
+                    self.ids[i],
+                    self.ips[i],
+                    self,
+                )
+            )
+            self.AL[i].receiver()
+        
+        
+        for i in range(0, len(self.ids)):
+            self.AL[i].key_exchange()
+
+
+    def __thread(self):
+        while True:
+            for msg in self.currentMSG:
+                counter_echos = 0
+                counter_readys = 0
+
+                for i in self.echos.values():
+                    if i == msg:
+                        counter_echos += 1
+                for i in self.readys.values():
+                    if i == msg:
+                        counter_readys += 1
+
+                if (
+                    counter_echos > math.floor((len(self.ids) + self.faulty) / 2)
+                ) and self.sentready is False:
+                    self.sentready = True
+
+                    # Broadcast to all a ready message
+                    for i in range(len(self.ids)):
+                        if msg not in self.currentMSG:
+                            self.currentMSG.append(msg)
+
+                        packet = {"MSG": msg, "FLAG": "READY"}
+                        self.AL[i].send(packet)
+
+                if counter_readys > self.faulty and self.sentready is False:
+                    self.sentready = True
+
+                    # Broadcast to all a ready message
+                    for i in range(len(self.ids)):
+                        # TODO I don't know if it's an error
+                        
+                        if msg not in self.currentMSG:
+                            self.currentMSG.append(msg)
+
+                        packet = {"MSG": msg, "FLAG": "READY"}
+                        self.AL[i].send(packet)
+
+                if counter_readys > 2 * self.faulty and self.delivered is False:
+                    self.delivered = True
+
+                    # used for statistics
+
+                    peak = self.eval.tracing_mem()
+                    logging.info(
+                        "-----MESSAGE DELIVERED, time: %s, size: %s",
+                        time.time() * 1000,
+                        peak,
+                    )
+
+                    print("-----MESSAGE DELIVERED:", msg)
+
+                    return
+
+            # Not to destroy performance
+            time.sleep(BREAK_TIME)
+
+    # Before starting broadcast, a process reads the ip addresses and ids of
+    # the other processes from its queue
+
+    # The message is sent using authenticated link abstractions, it's a string with a flag indicating
+    # the type {SEND,ECHO,READY}
+    def broadcast(self, message):
+        
+        logging.info(
+            "----- EVALUATION CHECKPOINT: broadcast start, time: %s -----",
+            time.time() * 1000,
+        )
+
+        # Create packet as a dictionary
+        packet = {"MSG": message, "FLAG": "SEND"}
+        for i in range(len(self.ids)):
+            if message not in self.currentMSG:
+                self.currentMSG.append(message)
+            self.AL[i].send(packet)
+        self.barrier.wait()
+
+    def deliver_send(self, msg, idn):
+        if msg["FLAG"] == "SEND" and idn == 1 and self.sentecho is False:
+            # Add the message if it's not yet received
+            if msg["MSG"] not in self.currentMSG:
+                self.currentMSG.append(msg["MSG"])
+            self.sentecho = True
+            if self.selfid == 1:
+                self.barrier.wait()
+
+            # create packet
+            packet = {"MSG": msg["MSG"], "FLAG": "ECHO"}
+            for i in range(len(self.ids)):
+                self.AL[i].send(packet)
+        elif idn != 1:
+            logging.info("PROCESS: %d is not the intended sender!", idn)
+
+    def deliver_echo(self, msg, idn):
+        if msg["FLAG"] == "ECHO" and idn not in self.echos:
+            if msg["MSG"] not in self.currentMSG:
+                self.currentMSG.append(msg["MSG"])
+            self.echos[idn] = msg["MSG"]
+
+            logging.info("PROCESS: --------ECHOS VALUE: %s:", self.echos)
+
+    def deliver_ready(self, msg, idn):
+        if msg["FLAG"] == "READY" and idn not in self.readys:
+            if msg["MSG"] not in self.currentMSG:
+                self.currentMSG.append(msg["MSG"])
+            self.readys[idn] = msg["MSG"]
+
+            logging.info("PROCESS: --------READYS VALUE: %s:", self.readys)
